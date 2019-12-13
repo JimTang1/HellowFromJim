4aea5fc3fcd0780103d3a0444999fc222625bca6
JimTang <after062111@byui.edu>
2019-12-12 18:24:19 -0700

    add my name

---
+++ src/com/company/HelloW.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class HelloW {
+
+    public void sayHello() {
+        System.out.println("Hellow this is Jim!");
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        HelloW hello = new HelloW();
+        hello.sayHello();
+    }
+}
